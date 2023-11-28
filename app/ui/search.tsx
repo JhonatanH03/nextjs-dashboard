8a30945901b7727ac841ec48c2e2ed36cf865e6b
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-11-28 16:22:54 -0400

    refactor(search): optimize search logic

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,21 @@
-'use client';
- 
+import { useState, useEffect } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({
+  placeholder,
+  query: initialQuery,
+}: {
+  placeholder: string;
+  query: string;
+}) {
   const searchParams = useSearchParams();
+  const [query, setQuery] = useState(initialQuery);
+
+  useEffect(() => {
+    setQuery(initialQuery);
+  }, [initialQuery]);
+
   function handleSearch(term: string) {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
@@ -16,9 +27,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-    console.log(term);
+    setQuery(term);
   }
- 
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -30,7 +41,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        value={query}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
