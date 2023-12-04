f48bb1a86c53d2316a1d646a8beb140eb8448642
stiven64 <martinezsosastiven@gmail.com>
2023-12-04 15:55:29 -0400

    chore(molecules): move edit-form to src/components/molecules

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import Form from '@/app/ui/invoices/edit-form';
+import Form from '@/src/components/molecules/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
