f2305c1692046fcb02315351400ed0b7f6490eeb
JhonatanH03 <147352690+JhonatanH03@users.noreply.github.com>
2023-12-05 11:26:03 -0400

    chore(atoms): organize atoms in src/components/atoms

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/src/components/atoms/fonts';
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '../../src/components/atoms/button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 
